3c5e2a50e60b9f91a6db8d4ac173866195fb1ec5
Yann Klein <yann.klein@me.com>
2020-05-29 13:58:26 +0900

    UX minor twitch

--- App.js
+++ App.js
@@ -78,7 +78,12 @@ export default class App extends React.Component {
           type,
           name,
         }
-        this.setState({ photo: source})
+        this.setState(
+          {
+            photo: source,
+            log: ""
+          }
+        )
       }
     });
   }
