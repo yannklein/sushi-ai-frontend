be8c952f3298dd98006f78bd880eb07c3688c0f5
Yann Klein <yann.klein@me.com>
2020-05-29 13:20:15 +0900

    build fo=rom former RN project

--- App.js
+++ App.js
@@ -1,36 +1,218 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import ImagePicker from 'react-native-image-picker'
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+const createFormData = (photo) => {
+  const data = new FormData();
+
+  data.append("file", {
+    name: photo.fileName || "image.jpg",
+    type: photo.type,
+    uri:
+      Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
+  });
+
+  return data;
+};
+
+const cloudinaryUpload = (photo) => {
+
+  console.log(photo);
+  const fromData = new FormData()
+  fromData.append('file', photo)
+  fromData.append('upload_preset', 'st4y3ops')
+  fromData.append("cloud_name", "yanninthesky")
+
+  fetch("https://api.cloudinary.com/v1_1/yanninthesky/upload", {
+      method: "post",
+      body: fromData
+    })
+    .then(res => res.json())
+    .then(dat => {
+      console.log("Upload okay!", dat)
+    })
+    .catch(err => {
+      console.log("An Error Occured While Uploading")
+    })
+};
+
+export default class App extends React.Component {
+
+  state = {
+    photo: null,
+    answer: {
+        result: "",
+        details: {}
+      },
+    log: "",
+    askBtnText: "Ask the AI"
+  }
+
+  handleChoosePhoto = () => {
+    const options = {
+      title: 'Select Photo',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    }
+
+    ImagePicker.showImagePicker(options, (response) => {
+      // console.log('Response = ', response);
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else {
+        const uri = response.uri;
+        const type = response.type;
+        const name = response.fileName;
+        const source = {
+          uri,
+          type,
+          name,
+        }
+        this.setState({ photo: source})
+      }
+    });
+  }
+
+
+  handleUploadPhoto = () => {
+
+    const { photo } = this.state;
+
+    // cloudinaryUpload(photo);
+    if (!photo) {
+      this.setState(
+          {
+            log: "Upload a picture first!"
+          }
+      );
+      return 0;
+    }
+    this.setState({askBtnText: "Analyzing..."});
+
+
+    fetch("https://yanns-ai.onrender.com/analyze", {
+      method: "POST",
+      body: createFormData(photo)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("upload succes", data);
+        this.setState(
+          {
+            answer: data,
+            log: "",
+            askBtnText: "Ask the AI"
+          }
+        );
+      })
+      .catch(error => {
+        console.log("upload error", error);
+        this.setState(
+          {
+            log: error.toString(),
+            askBtnText: "Ask the AI"
+          }
+        );
+      });
+  };
+
+  render() {
+    const { photo, answer, log, askBtnText } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.uploadSection}>
+          <Text style={styles.title}>What's the sushi?! 🍣</Text>
+          <Text style={styles.parag}> What kind of <Text style={{fontWeight: "bold"}}>sushi</Text> are you eating? Let the AI guess! </Text>
+          <TouchableOpacity style={styles.upload} onPress={this.handleChoosePhoto} >
+            <Text style={styles.uploadText}>Upload a sushi</Text>
+          </TouchableOpacity>
+          <View style={styles.photo}>
+            {photo && (
+              <Image
+                source={{ uri: photo.uri }}
+                style={{ width: 200, height: 200 }}
+              />
+            )}
+          </View>
+        </View>
+        <View style={styles.result}>
+          <Text style={styles.answer}> { (answer.result === "" ? '' : `That's a... ${answer.result[0].toUpperCase()}${answer.result.substring(1)} sushi!`) }</Text>
+          <Text style={styles.list}>{ (answer.result === "" ? '' : Object.keys(answer.details).map(key => `${key[0].toUpperCase()}${key.substring(1)} sushi - ${answer.details[key]}%\n`).join('') ) } </Text>
+        </View>
+        <Text style={styles.parag}> { log } </Text>
+        <TouchableOpacity style={styles.ask} onPress={this.handleUploadPhoto} >
+          <Text style={styles.askText} >{ askBtnText }</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginTop: 40,
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  uploadSection: {
+    flex: 1,
+    alignItems: 'center'
   },
-  instructions: {
+  title: {
+    fontSize: 32,
+    marginVertical: 32,
+    fontWeight: 'bold'
+  },
+  parag: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 24,
+  },
+  result: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 184
+  },
+  answer: {
+    fontSize: 24,
+    marginTop: 24,
+    marginBottom: 20,
+    fontWeight: 'bold'
+  },
+  list: {
+    color: '#333333',
+  },
+  ask: {
+    backgroundColor: '#FF4F64',
+    width: '100%',
+    paddingTop: 32,
+    paddingBottom: 40,
+    margin: 0
+  },
+  askText: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 24,
+  },
+  upload: {
+    borderColor: '#FF4F64',
+    borderWidth: 1,
+    padding: 16,
+    marginBottom: 24
+  },
+  uploadText: {
+    textAlign: 'center',
+    color: '#FF4F64',
   },
 });
